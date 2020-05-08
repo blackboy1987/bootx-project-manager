7eca5bdbc25cbcb22a2695210ca50e7cf99401a0
xial@plian.net <a12345678>
2020-05-08 17:35:22 +0800

    整合网关

--- src/main/java/com/bootx/controller/ProjectInfoController.java
+++ src/main/java/com/bootx/controller/ProjectInfoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
-@RequestMapping("/api/project")
+@RequestMapping("/project")
 public class ProjectInfoController extends BaseController {
 
   @Autowired
